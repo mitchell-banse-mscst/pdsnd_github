e764ed24d5593d63dea9e87b601ec5ea305eb5e7
Banse, Mitchell L <mitchell.banse@optum.com>
2021-11-17 13:13:53 -0600

    refactor: Added a print statement to the end of the code

--- bikeshare.py
+++ bikeshare.py
@@ -190,6 +190,7 @@ def main():
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
+            print("Thank you for using bikeshare")
             break
 
 
